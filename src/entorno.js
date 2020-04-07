b101910557bb61dcf32b4a06a8aabb118dc14c95
flomas <f_lomas@hotmail.com>
2020-04-07 10:07:38 -0500

    FL: Parameters for the URL reviewed, easier to handle like this

--- src/entorno.js
+++ src/entorno.js
@@ -1,19 +1,21 @@
-/** 
+/**
  * Copyright 2020, Ingenia, S.A.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * @author jamartin@ingenia.es
  */
 export default {
-    WS_HOST: '',
-    API_KEY: ''
+    WS_URL: 'https://127.0.0.1:8443',
+    //WS_HOST: '127.0.0.1',
+    //WS_PORT: '8443',
+    API_KEY: 'PutYourKey'
 }
--- src/services/casoservice.js
+++ src/services/casoservice.js
@@ -1,16 +1,16 @@
-/** 
+/**
  * Copyright 2020, Ingenia, S.A.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * @author jamartin@ingenia.es
  */
 import axios from "axios";
@@ -19,8 +19,8 @@ import Entorno from '../entorno';
 export default {
     enviarCaso(caso, preguntas) {
         return new Promise((resolve, reject) => {
-            try {                    
-                axios.post('https://'+Entorno.WS_HOST+':8443/caso', {
+            try {
+                axios.post(Entorno.WS_URL + '/caso', {
                     'caso':caso,
                     'preguntas': preguntas
                 })
@@ -29,12 +29,12 @@ export default {
                             resolve(respuesta.data);
                         } else {
                             reject('');
-                        }                    
+                        }
                     })
                     .catch((error) => {
                         console.log('Ha ocurrido un error al crear el caso: ', error);
                         reject('')
-                    });                
+                    });
             } catch (error) {
                 console.log('Ha ocurrido un error antes de crear el caso: ', error);
                 reject('')
